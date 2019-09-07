4dee63a558318cf68093404693636e5c09d990b6
SKCrawford <s.kyle.crawford@gmail.com>
2019-09-06 19:28:06 -0700

    Add reporting of port after starting the server

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
+import { Logger } from './logger';
 import { createServer } from './server';
 
 async function main() {
+  const logger = new Logger(main.name);
   const port = 8000;
   const server = createServer(port);
   server.listen(port);
+  logger.log(`Server started on port ${port}`);
 }
 
 main().catch(console.error);
