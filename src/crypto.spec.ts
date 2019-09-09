ce87137d52b7a76a21230723a6f96e236bec90d2
SKCrawford <s.kyle.crawford@gmail.com>
2019-09-08 18:23:01 -0700

    Add controller test and improve crypto tests' readability

--- src/crypto.spec.ts
+++ src/crypto.spec.ts
@@ -2,23 +2,27 @@ import { decrypt, encrypt } from './crypto';
 
 describe('crypto', () => {
   const decryptedMessage = 'A test message 123';
-  const encryptedMessage = 'U2FsdGVkX19RuXwXp4qSMN+a8ZQQkDJcYEod5MMfAiqNSRRueDKXolJIkv1aMo9R';
   const key = 'notsorandomtestkey';
 
   describe('encrypt', () => {
-    it('encrypts a simple string', () => {
+    it('encrypts a simple string', done => {
       encrypt(decryptedMessage, key)
         .then(msg => {
-          expect(msg).toEqual(encryptedMessage);
+          expect(msg).not.toEqual(decryptedMessage);
+          done();
         });
     })
   })
 
   describe('decrypt', () => {
-    it('decrypts a simple string', () => {
-      decrypt(decryptedMessage, key)
-        .then(msg => {
-          expect(msg).toEqual(decryptedMessage);
+    it('decrypts a simple string', done => {
+      encrypt(decryptedMessage, key)
+        .then(encMsg => {
+          decrypt(encMsg, key)
+            .then(decMsg => {
+              expect(decMsg).toEqual(decryptedMessage);
+              done();
+            });
         });
     })
   })
